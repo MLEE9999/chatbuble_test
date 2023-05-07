5429becc3102e79b65f538a757841eaaadce9699
MLEE9999 <101166389+MLEE9999@users.noreply.github.com>
2023-05-08 00:21:52 +0900

    채팅창 테스트화면

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,148 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        debugShowCheckedModeBanner: false,
+        home: _MyHomePageState()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _MyHomePageState extends StatefulWidget {
+  const _MyHomePageState({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<_MyHomePageState> createState() => _MyHomePageStateState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyHomePageStateState extends State<_MyHomePageState> {
+  bool isuser = false;
+  final List<String> chat = <String>['1','2','3','4','5'];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+        appBar: AppBar(
+          automaticallyImplyLeading: false, // 뒤로가기 버튼 제거
+          backgroundColor: Colors.white, // 상단 바 배경색을 흰색으로 설정
+          title: Text('ChatBot', style: TextStyle(color: Colors.grey)), // 상단 바 글자색을 검정색으로 설정
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: OutlinedButton(
+                  onPressed: (){
+
+                  }, child: Text('채팅내역', style: TextStyle(color: Colors.black, fontSize: 20),)),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  //reverse: false,
+                  itemCount: chat.length,
+                  itemBuilder: (context, index){
+                    return Row(
+                      mainAxisAlignment: isuser ? MainAxisAlignment.start : MainAxisAlignment.end,
+                      children: [
+                        Container(
+                            decoration: BoxDecoration(
+                                color: isuser ? Colors.green[100] : Colors.grey[300],
+                                borderRadius: BorderRadius.circular(8)
+                            ),child: Text('${chat[index]}',style: TextStyle(color: Colors.black),)
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              '${chat[index]}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 4.0),
+                            Text(
+                                '${chat[index]}'
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+              flex: 9,),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: TextField(
+
+                      ),
+                    ),
+                    flex: 8,),
+                  Expanded(
+                    child: IconButton(
+                      onPressed: (){
+                        setState(() {
+                          isuser=true;
+                          chat.add('new message');
+                          print(isuser);
+                        });
+                      },
+                      icon: Icon(Icons.arrow_back),),
+                  )
+                ],
+              ),
+              flex: 1,)
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar( // 하단 바
+            height: 60, // 높이 60
+            child: Row( // 가로 정렬
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 일정 간격을 두고 정렬
+                children: [
+                  IconButton(
+                      onPressed: (){
+                        //Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage(UserNum: widget.UserNum)));
+                      },
+                      icon: Icon(Icons.home_outlined)
+                  ),
+                  IconButton(
+                      onPressed: (){
+                        //Navigator.of(context).push(MaterialPageRoute(builder: (context) => MyApp(userNum: widget.UserNum)));
+                      },
+                      icon: Icon(Icons.event_note_outlined)
+                  ),
+                  IconButton(
+                      onPressed: (){
+
+                      },
+                      icon: Icon(Icons.chat_outlined, color: Colors.blue)
+                  ),
+                  IconButton(
+                      onPressed: (){
+                        //Navigator.of(context).push(MaterialPageRoute(builder: (context) => my_page(userNum: widget.UserNum)));
+                      },
+                      icon: Icon(Icons.list_alt_outlined)
+                  )
+                ]
+            )
+        )
     );
   }
 }
